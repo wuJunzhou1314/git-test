5202382de15409192c157a1510c1052d52d64ce9
wjz <wjz@sina.cn>
2022-06-21 15:28:21 +0800

    master hb commit!

--- src/main/java/com/zpark/git/TestGit.java
+++ src/main/java/com/zpark/git/TestGit.java
@@ -4,6 +4,7 @@ public class TestGit {
 
     public static void main(String[] args) {
         System.out.println("IDEA test Git! --master");
+        System.out.println("IDEA test Git! --master1");
         System.out.println("IDEA test Git! --hot-fix");
     }
 
