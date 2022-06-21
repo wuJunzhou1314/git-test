7f8f1d209a5f0ff75b7cc0cb1739fb9fe08d1b45
wjz <wjz@sina.cn>
2022-06-21 15:29:26 +0800

    hot-fix hb commit!

--- src/main/java/com/zpark/git/TestGit.java
+++ src/main/java/com/zpark/git/TestGit.java
@@ -5,6 +5,7 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("IDEA test Git! --master");
         System.out.println("IDEA test Git! --hot-fix");
+        System.out.println("IDEA test Git! --hot-fix1");
     }
 
 }
